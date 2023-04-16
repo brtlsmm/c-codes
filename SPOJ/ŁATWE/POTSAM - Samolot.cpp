dda006afd92d412dae3dc4b884294f329d556f48
brtlsmm <129326902+brtlsmm@users.noreply.github.com>
2023-04-16 15:48:58 +0200

    Create POTSAM - Samolot.cpp

---
+++ SPOJ/ŁATWE/POTSAM - Samolot.cpp
@@ -0,0 +1,16 @@
+// POTSAM - Samolot.cpp
+// by brtlsm
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, k1, n2, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+
+    long long int suma = (n1 * k1) + (n2 * k2);
+    cout << suma;
+}
+
